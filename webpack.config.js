7a64a05874f1e1888ab0945523141d4a525f27d3
AyyKamp <arda513@hotmail.de>
2019-05-28 14:58:47 +0200

    maybe?

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  chainWebpack: config => {
+    /* ... */
+    config.module
+      .rule('worker')
+        .test(/worker\.js$/)
+        .use('worker-loader')
+          .loader('worker-loader')
+      .end()
+    /* ... */
+  }
+}
